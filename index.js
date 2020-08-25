ef5955471026f7f4fb65de950f6be284adad1f6f
Fatih Cakmak <cakmakfatih@outlook.com.tr>
2020-08-25 21:06:02 +0300

    removed unnecessary lines

--- index.js
+++ index.js
@@ -5,10 +5,10 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import wa from 'webassemblyjs';
+// import wa from 'webassemblyjs';
 
-if (!global.WebAssembly) {
-    global.WebAssembly = wa;
-}
+// if (!global.WebAssembly) {
+//     global.WebAssembly = wa;
+// }
 
 AppRegistry.registerComponent(appName, () => App);
